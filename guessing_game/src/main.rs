066ea8154a6c80ef345ff7a64c49f3b78631a59a
Daniel Cender <danielcender@gmail.com>
2025-01-30 21:03:01 -0600

    finished guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,25 +1,37 @@
-use std::{io, cmp::Ordering};
 use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Without the "turbofish" ::<u32> the parse() method doesn't know what to parse to,
+        //  causing a weird comparison failure on line 27 when we expect the guess to match the
+        //  secret number.
+        let guess: u32 = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Greater => println!("Too high!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
