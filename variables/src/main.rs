0e61220995e61e73a24bd2f01ba83ac3e9e753f3
Daniel Cender <danielcender@gmail.com>
2025-02-27 23:48:56 -0600

    variables chapter

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    // todo: Fill out with content from https://doc.rust-lang.org/book/ch03-00-common-programming-concepts.html
+    let mut x = 5;
+    println!("Value of x is: {x}");
+    x = 3;
+    println!("X is now: {x}");
+
+    println!("Constant is: {THREE_HOURS_IN_SECONDS}");
+}
